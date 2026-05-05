a7abf41a1e5b1489461a7934378563497dae57ac
nmacharla889 <nmacharla889@users.noreply.github.com>
2026-05-05 22:51:42 +1000

    Bugs

--- macros/incremental_filter.sql
+++ macros/incremental_filter.sql
@@ -1,5 +1,9 @@
 {% macro incremental_filter(column_name, days=3) %}
   {% if is_incremental() %}
-    WHERE {{ column_name }} > (select max({{ column_name }}) from {{ this }})
+    WHERE {{ column_name }} > (
+      SELECT max_val FROM (
+        SELECT MAX({{ column_name }}) AS max_val FROM {{ this }}
+      )
+    )
   {% endif %}
 {% endmacro %}
